b6f1046616d17d9df520a29b6613d2765a3691d4
Artem Lukin <artyom.lukin98@gmail.com>
2021-09-25 18:04:49 +0200

    Added ability to get IP multiple times Changed the style

--- popup.js
+++ popup.js
@@ -1,28 +1,31 @@
-const request = new Request('https://api.myip.com');
-
 let ip_text = document.getElementById("ip");
 let country_text = document.getElementById("country");
 
 function setIP() {
-    
-    //ip_text.innerHTML = "ip";
-    //country_text.innerHTML = "country";
+  const request = new Request("https://api.myip.com");
 
-    fetch(request)
-        .then(response => {
-            return response.json();
-        })
-        .then(dataIP => {
-            
-            let ip = dataIP.ip;
-            let country = dataIP.country;
-            ip_text.innerHTML = ip;
-            country_text.innerHTML = country;
-        });
-    
+  clearText(ip_text, country_text);
+
+  fetch(request)
+    .then((response) => {
+      return response.json();
+    })
+    .then((dataIP) => {
+      let ip = dataIP.ip;
+      let country = dataIP.country;
+      ip_text.innerHTML = ip;
+      country_text.innerHTML = country;
+      ip_text.style.display = "initial";
+      country_text.style.display = "initial";
+    });
+}
+
+function clearText(...fields) {
+  for (let field of fields) {
+    field.innerText = "";
+    field.style.display = "none";
+  }
 }
 
 let getIP = document.getElementById("getIP");
 getIP.addEventListener("click", setIP);
-
-
